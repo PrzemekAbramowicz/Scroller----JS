020bf2602544c2d43507fc59c644850edf8cce6f
PrzemekAbramowicz <65355720+PrzemekAbramowicz@users.noreply.github.com>
2020-10-27 15:34:19 +0100

    Add Scroller class and use it

---
+++ JS/Scroller.js
@@ -0,0 +1,64 @@
+class Scroller {
+    constructor(rootSelector) {
+        const rootElement = document.querySelector(rootSelector)
+        this.sections = document.querySelectorAll('section')
+        const sectionsArr = [...this.sections]
+        
+        const currentSectionIndex = sectionsArr.findIndex((elem) => {
+            return this.isScrolledIntoView(elem)
+        })
+
+        this.currentSectionIndex = Math.max(currentSectionIndex , 0)
+
+        this.isThrottled = false
+
+        this.isScrolledIntoView(this.sections[0])
+    }
+
+    isScrolledIntoView(element){
+        const rect = element.getBoundingClientRect();
+        const elementTop = rect.top
+        const elementBottom = rect.bottom
+
+        const isVissible = (elementTop >= 0) && (elementBottom <= window.innerHeight)
+
+        return isVissible 
+    }
+
+    listenScroll = (event) => {
+        if(this.isThrottled) return
+        this.isThrottled = true
+
+        setTimeout(() => {
+            this.isThrottled = false
+        }, 1000)
+
+        const direction = event.wheelDelta < 0 ? 1 : -1
+
+        this.scroll(direction);
+    } 
+
+    scroll = (direction) => {
+        if (direction === 1) {
+            const isLastSection = this.currentSectionIndex === this.sections.length -1;
+            if(isLastSection) return;
+        } else if (direction === -1) {
+            const firstSection = this.currentSectionIndex === 0;
+            if(firstSection) return;
+        }
+
+        this.currentSectionIndex = this.currentSectionIndex + direction;
+
+      
+        this.scrollToCurrentSection();
+
+    }
+
+    scrollToCurrentSection = () => {
+        this.sections[this.currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    }
+
+}
--- JS/main.js
+++ JS/main.js
@@ -1,45 +1,6 @@
 document.addEventListener('DOMContentLoaded' , function() { 
-console.log('hello world');
 
-    const rootElement = document.querySelector('.scroller')
-    const sections = document.querySelectorAll('section')
-    let currentSectionIndex = 0
-    let isThrottled = false
-
-    document.addEventListener('mousewheel' , function(event) {
-       
-        if(isThrottled) return
-        isThrottled = true
-
-        setTimeout(function(){
-            isThrottled = false
-        }, 1000)
-
-        const direction = event.wheelDelta < 0 ? 1 : -1
-
-        scroll(direction);
-    })
-
-    function scroll(direction) {
-        if (direction === 1) {
-            const isLastSection = currentSectionIndex === sections.length -1;
-            if(isLastSection) return;
-        } else if (direction === -1) {
-            const firstSection = currentSectionIndex === 0;
-            if(firstSection) return;
-        }
-
-        currentSectionIndex = currentSectionIndex + direction;
-
-      
-        scrollToCurrentSection();
-
-    }
-
-    function scrollToCurrentSection () {
-        sections[currentSectionIndex].scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        })
-    }
+    const scroller = new Scroller('.scroller');
+    
+    document.addEventListener('mousewheel' , scroller.listenScroll)
 })
