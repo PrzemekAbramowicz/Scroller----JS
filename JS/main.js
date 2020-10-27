346e39848f8fbca79f3737cad0fdd1f9ebca9a62
PrzemekAbramowicz <65355720+PrzemekAbramowicz@users.noreply.github.com>
2020-10-27 17:46:22 +0100

    Add swiper to detect swipes on touchable devices

--- JS/main.js
+++ JS/main.js
@@ -3,4 +3,6 @@ document.addEventListener('DOMContentLoaded' , function() {
     const scroller = new Scroller('.scroller');
     
     document.addEventListener('wheel' , (event) => scroller.listenScroll(event))
+    document.addEventListener('swipeUp' , () => scroller.scroll(1))
+    document.addEventListener('swipeDown' , () => scroller.scroll(-1))
 })
